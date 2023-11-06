9194ca7c52286767da84ef7fcebe3fec40c14eb4
Ankit Singh <89828727+AnkitSin24@users.noreply.github.com>
2023-11-06 11:24:37 +0530

    Update g.py
    add some functionality and change color

--- g.py
+++ g.py
@@ -1,110 +1,55 @@
-
 from tkinter import *
 
-expression = ""
-
 def press(num):
-	
-	global expression
-
-	expression = expression + str(num)
-
-	equation.set(expression)
+    current = equation.get()
+    equation.set(current + str(num))
 
 def equalpress():
-	
-	try:
-
-		global expression
-
-	
-		total = str(eval(expression))
-
-		equation.set(total)
-
-		
-		expression = ""
-
-	
-	except:
-
-		equation.set(" error ")
-		expression = ""
-
+    try:
+        current = equation.get()
+        result = str(eval(current))
+        equation.set(result)
+    except:
+        equation.set("Error")
 
 def clear():
-	global expression
-	expression = ""
-	equation.set("")
-
-
+    equation.set("")
 
 if __name__ == "__main__":
-	gui = Tk()
+    gui = Tk()
+    gui.title("Calculator")
+    gui.geometry("300x400")
+    gui.configure(bg="lightgray")
 
-	gui.configure(background="light green")
- 
-	gui.title(" calculator")
-	gui.geometry("270x150")
+    equation = StringVar()
 
+    entry_field = Entry(gui, textvariable=equation, font=("Helvetica", 20), bd=10, insertwidth=4, width=14, justify="right")
+    entry_field.grid(row=0, column=0, columnspan=4, padx=10, pady=10, ipadx=10, ipady=10)
 
-	equation = StringVar()
+    buttons = [
+        '7', '8', '9', '/',
+        '4', '5', '6', '*',
+        '1', '2', '3', '-',
+        'C', '0', '=', '+'
+    ]
 
-	
-	expression_field = Entry(gui, textvariable=equation)
+    row_val = 1
+    col_val = 0
 
+    for button in buttons:
+        button_frame = Frame(gui, bg="lightgray")
+        button_frame.grid(row=row_val, column=col_val, padx=5, pady=5)
+        button = Button(button_frame, text=button, font=("Helvetica", 16), height=2, width=5, bd=5, bg="lightblue", activebackground="lightcyan")
+        button.pack(fill=BOTH, expand=True)
+        if button.cget("text") == '=':
+            button.bind("<Button-1>", lambda event: equalpress())
+        elif button.cget("text") == 'C':
+            button.bind("<Button-1>", lambda event: clear())
+        else:
+            button.bind("<Button-1>", lambda event, key=button.cget("text"): press(key))
+        col_val += 1
+        if col_val > 3:
+            col_val = 0
+            row_val += 1
 
-	expression_field.grid(columnspan=4, ipadx=70)
-
-	button1 = Button(gui, text=' 1 ', fg='black', command=lambda: press(1), height=1, width=7)
-	button1.grid(row=2, column=0)
-
-	button2 = Button(gui, text=' 2 ', fg='black',command=lambda: press(2), height=1, width=7)
-	button2.grid(row=2, column=1)
-
-	button3 = Button(gui, text=' 3 ', fg='black', command=lambda: press(3), height=1, width=7)
-	button3.grid(row=2, column=2)
-
-	button4 = Button(gui, text=' 4 ', fg='black', command=lambda: press(4), height=1, width=7)
-	button4.grid(row=3, column=0)
-
-	button5 = Button(gui, text=' 5 ', fg='black', command=lambda: press(5), height=1, width=7)
-	button5.grid(row=3, column=1)
-
-	button6 = Button(gui, text=' 6 ', fg='black', bg='red',command=lambda: press(6), height=1, width=7)
-	button6.grid(row=3, column=2)
-
-	button7 = Button(gui, text=' 7 ', fg='black', bg='red',command=lambda: press(7), height=1, width=7)
-	button7.grid(row=4, column=0)
-
-	button8 = Button(gui, text=' 8 ', fg='black', command=lambda: press(8), height=1, width=7)
-	button8.grid(row=4, column=1)
-
-	button9 = Button(gui, text=' 9 ', fg='black', bg='red',command=lambda: press(9), height=1, width=7)
-	button9.grid(row=4, column=2)
-
-	button0 = Button(gui, text=' 0 ', fg='black', command=lambda: press(0), height=1, width=7)
-	button0.grid(row=5, column=0)
-
-	plus = Button(gui, text=' + ', fg='black', command=lambda: press("+"), height=1, width=7)
-	plus.grid(row=2, column=3)
-
-	minus = Button(gui, text=' - ', fg='black', bg='red',command=lambda: press("-"), height=1, width=7)
-	minus.grid(row=3, column=3)
-
-	multiply = Button(gui, text=' * ', fg='black', bg='red',command=lambda: press("*"), height=1, width=7)
-	multiply.grid(row=4, column=3)
-
-	divide = Button(gui, text=' / ', fg='black', command=lambda: press("/"), height=1, width=7)
-	divide.grid(row=5, column=3)
-
-	equal = Button(gui, text=' = ', fg='black', command=equalpress, height=1, width=7)
-	equal.grid(row=5, column=2)
-
-	clear = Button(gui, text='Clear', fg='black', command=clear, height=1, width=7)
-	clear.grid(row=5, column='1')
-
-	Decimal= Button(gui, text='.', fg='black',command=lambda: press('.'), height=1, width=7)
-	Decimal.grid(row=6, column=0)
-	
-	gui.mainloop()
+    gui.mainloop()
